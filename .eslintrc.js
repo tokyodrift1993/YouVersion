903a7c6836550a8700475af4da305ed3feb58ec7
DRiiFT KiinG <driiftkiing@driiftkiing.ch>
2023-06-09 20:14:56 +0200

    (feat) configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort', 'unused-imports'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: false,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        printWidth: 140,
+      },
+    ],
+    indent: 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', {avoidEscape: true}],
+    semi: ['error', 'always'],
+    '@typescript-eslint/no-empty-interface': 'warn',
+    'no-duplicate-imports': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-console': ['error', {allow: ['log', 'warn', 'error', 'table']}],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', {functions: false}],
+    'eol-last': ['error', 'always'],
+  },
+};
