3e4fcb2fa9f19cdd6ad6f37056e528ebca44c91a
DRiiFT KiinG <driiftkiing@driiftkiing.ch>
2023-06-11 21:14:09 +0200

    (feat) ts: use target esnext

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     node: true,
     commonjs: true,
-    es2021: true,
+    es2022: true,
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
